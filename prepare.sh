ed0b9b24ebc717e9e40a5cb17200ead83de4d813
Jonghyeon Kim <tome01@ajou.ac.kr>
2021-12-27 17:08:25 +0900

    fixed typo

--- prepare.sh
+++ prepare.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Install packages to use virt-python
-# apt-get install virtualenv python3.8-venv
+apt-get install virtualenv python3.8-venv
 
 # Cretae a virtual environment for Python 3.8
-# python3 -m venv my_env/
+python3 -m venv my_env/
 
 # Set the version of virtual python
-# virtualenv env/ --python=python3.8
+virtualenv my_env/ --python=python3.8
 
 # Activate virt-python
 source my_env/bin/activate
