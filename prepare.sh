4287a7a9bd995da800d9aec0e4f479ba65572a9b
Jonghyeon Kim <tome01@ajou.ac.kr>
2021-12-27 17:01:50 +0900

    added script to prepare train model

---
+++ prepare.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Install packages to use virt-python
+# apt-get install virtualenv python3.8-venv
+
+# Cretae a virtual environment for Python 3.8
+# python3 -m venv my_env/
+
+# Set the version of virtual python
+# virtualenv env/ --python=python3.8
+
+# Activate virt-python
+source my_env/bin/activate
